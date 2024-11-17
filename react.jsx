034b2b8d632257e235de7bd3c6106b7406ac26bf
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-11-17 16:39:42 +0500

    Create react.jsx

---
+++ react.jsx
@@ -0,0 +1,7 @@
+export default function Button({ bg, color, text })
+{
+  // Let's go!
+  return (
+    <button style=`background-color: {bg}; color: {color}` value=`Background color: {bg}, color: {color}`></button>
+  )
+}
