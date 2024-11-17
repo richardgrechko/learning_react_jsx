d90dfe072fb323dc35f4417dc5d38fa0ebbde1ac
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-11-17 16:40:02 +0500

    Update react.jsx

--- react.jsx
+++ react.jsx
@@ -2,6 +2,6 @@ export default function Button({ bg, color, text })
 {
   // Let's go!
   return (
-    <button style=`background-color: {bg}; color: {color}` value=`Background color: {bg}, color: {color}`></button>
+    <button style=`background-color: {bg}; color: {color}` value=`Background color: {bg}, color: {color}, Text: {text}`></button>
   )
 }
